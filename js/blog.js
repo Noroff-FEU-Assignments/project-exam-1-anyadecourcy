9f5079c58fd659c2d046549a94fc4eb825eff9aa
anyadecourcy <anya.decourcy@gmail.com>
2021-05-02 11:07:15 +0200

    CSS
    Added more CSS to blog and blog info page

--- js/blog.js
+++ js/blog.js
@@ -1,4 +1,6 @@
 const blogContainer = document.querySelector(".blogPosts");
+
+
 const url = "https://anyadecourcy.no/wp-json/wp/v2/posts";
 let docTitle=document.title;
 
@@ -25,18 +27,22 @@ function createHTML(details){
     details.forEach(function(detail){
         console.log(detail);
 
+        
+
         blogContainer.innerHTML += 
             `
-            
-            <a href = "blog-info.html?id=${detail.id}">
-                 <h1>${detail.title.rendered}</h1>
-            </a>
-            <date>${detail.date}</date>
-          
-            <p>${detail.excerpt.rendered}</p>
+                <div class="data"> 
+                <a href = "blog-info.html?id=${detail.id}">
+                    <h1>${detail.title.rendered}</h1>
+                    <div class="miniPicture">${detail.content.rendered}</div>
+                </a>
+                <date>${detail.date}</date>
+                <p class="excerpt">${detail.excerpt.rendered}</p>
+                </div>
 
-            
+        
             `
+           
     
 
 
